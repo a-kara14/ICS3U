67e730870f50b9ef43fcc13b3b822e525c2a642c
323637330 <323637330@C-H6304V1.tdsb.on.ca>
2016-03-02 14:44:50 -0500

    created hello world a program that prints hello world

---
+++ ICS3U/src/karaalp/HelloWorld.java
@@ -0,0 +1,19 @@
+package karaalp;
+
+/*HelloWorld.java
+ * this program prints Hello world
+ * Adem Karaalp
+ * March 2 2016
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// this prints hello world to the console
+	
+		System.out.println ("Hello World");
+		
+		
+	}
+
+}
