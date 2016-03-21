28a53e74ffb053a0b452c852137f227a0e1940f5
323637330 <323637330@C-H6304V1.tdsb.on.ca>
2016-03-21 14:34:28 -0400

    this program uses the quadratic formula, it gets 3 numbers from the user and puts it into the formula and finds the roots

---
+++ ICS3U/src/karaalp/QuadrationEquation.java
@@ -0,0 +1,48 @@
+package karaalp;
+
+/*QuadraticEquation.java
+ * This program uses the quadratic formula
+ * Adem Karaalp
+ * March 21, 2016
+ */
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class QuadrationEquation {
+
+	public static void main(String[] args) {
+		
+		//variables
+		double a, b, c, x, y;
+		
+		Scanner scan = new Scanner (System.in);
+		DecimalFormat df = new DecimalFormat("0.00");
+		
+		//input
+		System.out.println("Please add in a number");
+		a = Double.parseDouble(scan.nextLine());
+		System.out.println("Please add in a number");
+		b = Double.parseDouble(scan.nextLine());
+		System.out.println("Please add in a number");
+		c = Double.parseDouble(scan.nextLine());
+
+		x = ( -b + (Math.sqrt(Math.pow(b, 2) - 4 * a * c)))/(2*a);
+		y = ( -b - (Math.sqrt(Math.pow(b, 2) - 4 * a * c)))/(2*a);
+		
+		//output
+		if (Math.pow(b, 2) - 4 * a * c < 0)
+		{ 
+			System.out.println("There is no roots");
+		}
+		else if (Math.pow(b, 2) - 4 * a * c == 0)
+		{ 
+			System.out.println("there is only one root and it is " + df.format(x));
+		}
+		else 
+		{
+			System.out.println("the roots are " + df.format(x) + " and " + df.format(y));
+		}
+	}
+
+}
