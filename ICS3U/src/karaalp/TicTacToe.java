d14858e15a26d9d1bd28df7d129d77d8d4068720
323637330 <323637330@C-H6304V1.tdsb.on.ca>
2016-05-13 14:50:36 -0400

    Fixed the part when the opponent adds the same coordinate as another player also added play again

--- ICS3U/src/karaalp/TicTacToe.java
+++ ICS3U/src/karaalp/TicTacToe.java
@@ -1,4 +1,5 @@
 package karaalp;
+
 /*TicTacToe.java
  * Tic Tac toe game by entering coordinates
  * Adem Karaalp
@@ -7,157 +8,211 @@ package karaalp;
 import java.util.Scanner;
 
 public class TicTacToe {
-public static void main(String[] args) {
-		
-		//variables
-		String [][] tttBoard=new String [3][3];
-		tttBoard[0][0]="";
-		tttBoard[0][1]="";
-		tttBoard[0][2]="";
-		tttBoard[1][0]="";
-		tttBoard[1][1]="";
-		tttBoard[1][2]="";
-		tttBoard[2][0]="";
-		tttBoard[2][1]="";
-		tttBoard[2][2]="";
-		boolean win=false;
+	public static void main(String[] args) {
+
+		// declaring variables
+		String[][] tttBoard = new String[3][3];
+		boolean[][] checks = new boolean[3][3];
+		boolean x = false;
+		boolean o = false;
+		tttBoard[0][0] = "";
+		tttBoard[0][1] = "";
+		tttBoard[0][2] = "";
+		tttBoard[1][0] = "";
+		tttBoard[1][1] = "";
+		tttBoard[1][2] = "";
+		tttBoard[2][0] = "";
+		tttBoard[2][1] = "";
+		tttBoard[2][2] = "";
+		boolean win = false;
 		int row;
 		int column;
-		
-		Scanner scan=new Scanner (System.in);
-		
-		System.out.println("Alright lets play Tic Tac Toe");
-		System.out.println("Rules are simple you just plot coordinates");
-		System.out.println("you can enter from 1-3, first you enter the row you want, then you enter the column");
-		
-		while (win==false)
-		{
-			//X's turn
-			System.out.println("First player what are the coordinates you want to place the X(e.g. 3 1)");
-		
-			row=scan.nextInt();
-			column=scan.nextInt();
-			
-			//planting x
-			tttBoard[row-1][column-1]="X";
-			
-			//display's board
-			System.out.println(tttBoard[0][0]+"     "+tttBoard[0][1]+"     "+tttBoard[0][2]);
-			System.out.println(tttBoard[1][0]+"     "+tttBoard[1][1]+"     "+tttBoard[1][2]);
-			System.out.println(tttBoard[2][0]+"     "+tttBoard[2][1]+"     "+tttBoard[2][2]);
-		
-			//check's for winner
-			if(tttBoard[0][0]==tttBoard[0][1]&&tttBoard[0][0]==tttBoard[0][2]&&!tttBoard[0][2].equals(""))
-			{
-				System.out.println(tttBoard[0][0]+" wins!");
-				win=true;
-			}
-			else if(tttBoard[1][0]==tttBoard[1][1]&&tttBoard[1][1]==tttBoard[1][2]&&!tttBoard[1][2].equals(""))
-			{
-				System.out.println(tttBoard[1][0]+" wins!");
-				win=true;
-			}
-			else if(tttBoard[2][0]==tttBoard[2][1]&&tttBoard[2][0]==tttBoard[2][2]&&!tttBoard[2][2].equals(""))
-			{
-				System.out.println(tttBoard[2][0]+" wins!");
-				win=true;
-			}
-			else if(tttBoard[0][0]==tttBoard[1][0]&&tttBoard[0][0]==tttBoard[2][0]&&!tttBoard[1][0].equals(""))
-			{
-				System.out.println(tttBoard[1][0]+" wins!");
-				win=true;
-			}
-			else if(tttBoard[1][0]==tttBoard[1][1]&&tttBoard[1][0]==tttBoard[1][2]&&!tttBoard[1][2].equals(""))
-			{
-				System.out.println(tttBoard[1][0]+" wins!");
-				win=true;
-			}
-			else if(tttBoard[2][0]==tttBoard[2][1]&&tttBoard[2][0]==tttBoard[2][2]&&!tttBoard[2][2].equals(""))
-			{
-				System.out.println(tttBoard[2][0]+" wins!");
-				win=true;
-			}
-			else if(tttBoard[0][0]==tttBoard[1][1]&&tttBoard[0][0]==tttBoard[2][2]&&!tttBoard[2][2].equals(""))
-			{
-				System.out.println(tttBoard[2][2]+" wins!");
-				win=true;
-			}
-			else if(tttBoard[0][2]==tttBoard[1][1]&&tttBoard[0][2]==tttBoard[2][0]&&!tttBoard[0][2].equals(""))
-			{
-				System.out.println(tttBoard[0][0]+" wins!");
-				win=true;
-			}
-			else
-			{
-				win=false;
-			}
-			
-			//O's turn
-			if (win==false){
-			System.out.println("second player what are the coordinates you want to place the O(e.g. 3 1)");
-		
-			row=scan.nextInt();
-			column=scan.nextInt();
-			
-			//planting O
-			tttBoard[row-1][column-1]="O";
-		
-			//show's board
-			System.out.println(tttBoard[0][0]+"     "+tttBoard[0][1]+"     "+tttBoard[0][2]);
-			System.out.println(tttBoard[1][0]+"     "+tttBoard[1][1]+"     "+tttBoard[1][2]);
-			System.out.println(tttBoard[2][0]+"     "+tttBoard[2][1]+"     "+tttBoard[2][2]);
-			
-			//check's for winner
-			if(tttBoard[0][0]==tttBoard[0][1]&&tttBoard[0][0]==tttBoard[0][2]&&!tttBoard[0][2].equals(""))
-			{
-				System.out.println(tttBoard[0][0]+" wins!");
-				win=true;
-			}
-			else if(tttBoard[1][0]==tttBoard[1][1]&&tttBoard[1][1]==tttBoard[1][2]&&!tttBoard[1][2].equals(""))
-			{
-				System.out.println(tttBoard[1][0]+" wins!");
-				win=true;
-			}
-			else if(tttBoard[2][0]==tttBoard[2][1]&&tttBoard[2][0]==tttBoard[2][2]&&!tttBoard[2][2].equals(""))
-			{
-				System.out.println(tttBoard[2][0]+" wins!");
-				win=true;
-			}
-			else if(tttBoard[0][0]==tttBoard[1][0]&&tttBoard[0][0]==tttBoard[2][0]&&!tttBoard[1][0].equals(""))
-			{
-				System.out.println(tttBoard[1][0]+" wins!");
-				win=true;
-			}
-			else if(tttBoard[1][0]==tttBoard[1][1]&&tttBoard[1][0]==tttBoard[1][2]&&!tttBoard[1][2].equals(""))
-			{
-				System.out.println(tttBoard[1][0]+" wins!");
-				win=true;
-			}
-			else if(tttBoard[2][0]==tttBoard[2][1]&&tttBoard[2][0]==tttBoard[2][2]&&!tttBoard[2][2].equals(""))
-			{
-				System.out.println(tttBoard[2][0]+" wins!");
-				win=true;
-			}
-			else if(tttBoard[0][0]==tttBoard[1][1]&&tttBoard[0][0]==tttBoard[2][2]&&!tttBoard[2][2].equals(""))
-			{
-				System.out.println(tttBoard[2][2]+" wins!");
-				win=true;
-			}
-			else if(tttBoard[0][2]==tttBoard[1][1]&&tttBoard[0][2]==tttBoard[2][0]&&!tttBoard[0][2].equals(""))
-			{
-				System.out.println(tttBoard[0][0]+" wins!");
-				win=true;
-			}
-			else
-			{
-				win=false;
-			}
-			if (!tttBoard[0][0].equals("") && !tttBoard[0][1].equals("") &&!tttBoard[0][2].equals("") && !tttBoard[1][0].equals("") &&!tttBoard[1][1].equals("") &&!tttBoard[1][2].equals("") &&!tttBoard[2][0].equals("") &&!tttBoard[2][1].equals("") &&!tttBoard[2][2].equals("")){
-				win = true;
-				System.out.println("Tie game!");
-			}
-		}
-		}
-	}
+		boolean playOrNah = true;
+		String play;
 
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Do you want to play tic tac toe?");
+		play = scan.nextLine();
+		if ( play.equalsIgnoreCase("yes")){
+			playOrNah = true;
+		}else {
+			playOrNah = false;
+		}
+
+		while (playOrNah) {
+			System.out.println("Alright lets play Tic Tac Toe");
+			System.out.println("The rules are simple, you enter the coordinate you want the X or the O to be");
+			System.out.println("Minimum row and column is 1 and maximum is 3");
+			while (!win) {
+				// X's turn
+				if (!o) {
+					System.out.println("First player enter the coordinates you would like to place the X(e.g. 3 1)");
+
+					row = scan.nextInt();
+					column = scan.nextInt();
+					if (!checks[row - 1][column - 1]) {
+						checks[row - 1][column - 1] = true;
+						// plant x
+						tttBoard[row - 1][column - 1] = "X";
+
+						// display board
+						System.out.println(tttBoard[0][0] + "\t" + tttBoard[0][1] + "\t" + tttBoard[0][2]);
+						System.out.println(tttBoard[1][0] + "\t" + tttBoard[1][1] + "\t" + tttBoard[1][2]);
+						System.out.println(tttBoard[2][0] + "\t" + tttBoard[2][1] + "\t" + tttBoard[2][2]);
+
+						// check for winner
+						if (tttBoard[0][0] == tttBoard[0][1] && tttBoard[0][0] == tttBoard[0][2]
+								&& !tttBoard[0][2].equals("")) {
+							System.out.println(tttBoard[0][0] + " wins!");
+							win = true;
+						} else if (tttBoard[1][0] == tttBoard[1][1] && tttBoard[1][1] == tttBoard[1][2]
+								&& !tttBoard[1][2].equals("")) {
+							System.out.println(tttBoard[1][0] + " wins!");
+							win = true;
+						} else if (tttBoard[2][0] == tttBoard[2][1] && tttBoard[2][0] == tttBoard[2][2]
+								&& !tttBoard[2][2].equals("")) {
+							System.out.println(tttBoard[2][0] + " wins!");
+							win = true;
+						} else if (tttBoard[0][0] == tttBoard[1][0] && tttBoard[0][0] == tttBoard[2][0]
+								&& !tttBoard[1][0].equals("")) {
+							System.out.println(tttBoard[1][0] + " wins!");
+							win = true;
+						} else if (tttBoard[1][0] == tttBoard[1][1] && tttBoard[1][0] == tttBoard[1][2]
+								&& !tttBoard[1][2].equals("")) {
+							System.out.println(tttBoard[1][0] + " wins!");
+							win = true;
+						} else if (tttBoard[2][0] == tttBoard[2][1] && tttBoard[2][0] == tttBoard[2][2]
+								&& !tttBoard[2][2].equals("")) {
+							System.out.println(tttBoard[2][0] + " wins!");
+							win = true;
+						} else if (tttBoard[0][0] == tttBoard[1][1] && tttBoard[0][0] == tttBoard[2][2]
+								&& !tttBoard[2][2].equals("")) {
+							System.out.println(tttBoard[2][2] + " wins!");
+							win = true;
+						} else if (tttBoard[0][2] == tttBoard[1][1] && tttBoard[0][2] == tttBoard[2][0]
+								&& !tttBoard[0][2].equals("")) {
+							System.out.println(tttBoard[0][0] + " wins!");
+							win = true;
+						} else {
+							win = false;
+						}
+						// checks for cats game
+						if (!tttBoard[0][0].equals("") && !tttBoard[0][1].equals("") && !tttBoard[0][2].equals("")
+								&& !tttBoard[1][0].equals("") && !tttBoard[1][1].equals("")
+								&& !tttBoard[1][2].equals("") && !tttBoard[2][0].equals("")
+								&& !tttBoard[2][1].equals("") && !tttBoard[2][2].equals("")) {
+							win = true;
+							System.out.println("Cat's game!");
+						}
+						x = false;
+					} else {
+						System.out.println("Those coordinates were already entered, please enter a different one.");
+						x = true;
+					}
+				}
+
+				// O's turn
+				if (!win && !x) { // checks if x has already won
+					System.out.println("Second player enter coordinates you would like to place the O(e.g. 3 1)");
+
+					row = scan.nextInt();
+					column = scan.nextInt();
+
+					if (!checks[row - 1][column - 1]) {
+						checks[row - 1][column - 1] = true;
+						// plant o
+						tttBoard[row - 1][column - 1] = "O";
+
+						// show board
+						System.out.println(tttBoard[0][0] + "\t" + tttBoard[0][1] + "\t" + tttBoard[0][2]);
+						System.out.println(tttBoard[1][0] + "\t" + tttBoard[1][1] + "\t" + tttBoard[1][2]);
+						System.out.println(tttBoard[2][0] + "\t" + tttBoard[2][1] + "\t" + tttBoard[2][2]);
+
+						// check for win
+						if (tttBoard[0][0] == tttBoard[0][1] && tttBoard[0][0] == tttBoard[0][2]
+								&& !tttBoard[0][2].equals("")) {
+							System.out.println(tttBoard[0][0] + " wins!");
+							win = true;
+						} else if (tttBoard[1][0] == tttBoard[1][1] && tttBoard[1][1] == tttBoard[1][2]
+								&& !tttBoard[1][2].equals("")) {
+							System.out.println(tttBoard[1][0] + " wins!");
+							win = true;
+						} else if (tttBoard[2][0] == tttBoard[2][1] && tttBoard[2][0] == tttBoard[2][2]
+								&& !tttBoard[2][2].equals("")) {
+							System.out.println(tttBoard[2][0] + " wins!");
+							win = true;
+						} else if (tttBoard[0][0] == tttBoard[1][0] && tttBoard[0][0] == tttBoard[2][0]
+								&& !tttBoard[1][0].equals("")) {
+							System.out.println(tttBoard[1][0] + " wins!");
+							win = true;
+						} else if (tttBoard[1][0] == tttBoard[1][1] && tttBoard[1][0] == tttBoard[1][2]
+								&& !tttBoard[1][2].equals("")) {
+							System.out.println(tttBoard[1][0] + " wins!");
+							win = true;
+						} else if (tttBoard[2][0] == tttBoard[2][1] && tttBoard[2][0] == tttBoard[2][2]
+								&& !tttBoard[2][2].equals("")) {
+							System.out.println(tttBoard[2][0] + " wins!");
+							win = true;
+						} else if (tttBoard[0][0] == tttBoard[1][1] && tttBoard[0][0] == tttBoard[2][2]
+								&& !tttBoard[2][2].equals("")) {
+							System.out.println(tttBoard[2][2] + " wins!");
+							win = true;
+						} else if (tttBoard[0][2] == tttBoard[1][1] && tttBoard[0][2] == tttBoard[2][0]
+								&& !tttBoard[0][2].equals("")) {
+							System.out.println(tttBoard[0][0] + " wins!");
+							win = true;
+						} else {
+							win = false;
+						}
+						// checks for cats game
+						if (!tttBoard[0][0].equals("") && !tttBoard[0][1].equals("") && !tttBoard[0][2].equals("")
+								&& !tttBoard[1][0].equals("") && !tttBoard[1][1].equals("")
+								&& !tttBoard[1][2].equals("") && !tttBoard[2][0].equals("")
+								&& !tttBoard[2][1].equals("") && !tttBoard[2][2].equals("")) {
+							win = true;
+							System.out.println("Cat's game!");
+						}
+						o = false;
+					} else {
+						System.out.println("Those coordinates were already entered, please enter different one.");
+						o = true;
+					}
+				}
+			}
+			System.out.println("Do you want to play again?");
+			scan.nextLine();
+			play = scan.nextLine();
+			if (play.equalsIgnoreCase("no")) {
+				playOrNah = false;
+			} else {
+				playOrNah = true;
+				tttBoard[0][0] = "";
+				tttBoard[0][1] = "";
+				tttBoard[0][2] = "";
+				tttBoard[1][0] = "";
+				tttBoard[1][1] = "";
+				tttBoard[1][2] = "";
+				tttBoard[2][0] = "";
+				tttBoard[2][1] = "";
+				tttBoard[2][2] = "";
+
+				checks[0][0] = false;
+				checks[0][1] = false;
+				checks[0][2] = false;
+				checks[1][0] = false;
+				checks[1][1] = false;
+				checks[1][2] = false;
+				checks[2][0] = false;
+				checks[2][1] = false;
+				checks[2][2] = false;
+
+				win = false;
+				playOrNah = true;
+				x = false;
+				o = false;
+			}
+
+		}
+		System.out.println("Guess we wont play :(");
+	}
 }
