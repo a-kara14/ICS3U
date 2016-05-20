4f6b60c1d6a7ce97d768a2a68664ca08a396a8dd
323637330 <323637330@C-H6304V1.tdsb.on.ca>
2016-05-20 14:35:09 -0400

    This program prints Mississippi using methods

---
+++ ICS3U/src/karaalp/Mississippi.java
@@ -0,0 +1,91 @@
+package karaalp;
+/**Mississippi.java
+ * This program prints Mississippi using methods
+ * Adem Karaalp 
+ * May 20 2016
+ */
+
+public class Mississippi {
+	
+	public static void main(String[] args) {
+		drawM();
+		System.out.println(" ");
+		drawI();
+		System.out.println(" ");
+		drawS();
+		System.out.println(" ");
+		drawS();
+		System.out.println(" ");
+		drawI();
+		System.out.println(" ");
+		drawS();
+		System.out.println(" ");
+		drawS();
+		System.out.println(" ");
+		drawI();
+		System.out.println(" ");
+		drawP();
+		System.out.println(" ");
+		drawP();
+		System.out.println(" ");
+		drawI();
+		System.out.println(" ");
+	}
+/**
+ * This Method Prints the letter 'P' in Mississppi
+ */
+	private static void drawP() {
+		System.out.println(" PPPPPPPPP   ");
+		System.out.println(" P        P  ");
+		System.out.println(" P         P ");
+		System.out.println(" P        P  ");
+		System.out.println(" PPPPPPPPP   ");
+		System.out.println(" P           ");
+		System.out.println(" P           ");
+		System.out.println(" P           ");
+		System.out.println(" P           ");
+
+	}
+	/**
+	 * This Method Prints the letter 'S' in Mississppi
+	 */
+
+	private static void drawS() {
+		System.out.println("   SSSSSSS  ");
+		System.out.println("  S       S ");
+		System.out.println(" S          ");
+		System.out.println("  S         ");
+		System.out.println("   SSSSSSS  ");
+		System.out.println("          S ");
+		System.out.println("           S");
+		System.out.println("          S ");
+		System.out.println("  SSSSSSSSS ");
+
+	}
+	/**
+	 * This Method Prints the letter 'I' in Mississppi
+	 */
+
+	private static void drawI() {
+		System.out.println("IIIIIIIIIIII");
+		System.out.println("     II     ");
+		System.out.println("     II     ");
+		System.out.println("     II     ");
+		System.out.println("     II     ");
+		System.out.println("     II     ");
+		System.out.println("IIIIIIIIIIII");
+	}
+	/**
+	 * This Method Prints the letter 'M' in Mississppi
+	 */
+
+	private static void drawM() {
+		System.out.println("M          M");
+		System.out.println("MM        MM");
+		System.out.println("M  M	M  M");
+		System.out.println("M   M  M   M");
+		System.out.println("M    MM    M");
+		System.out.println("M          M");
+		System.out.println("M          M");
+	}
+}
